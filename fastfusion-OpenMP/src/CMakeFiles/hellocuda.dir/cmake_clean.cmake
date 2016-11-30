71f53d1aef1dfa3e29cdd2fbd671941d1ac03f0e
sargam2805 <sargam.jain@iitgn.ac.in>
2016-11-30 21:21:58 +0530

    OK

--- fastfusion-OpenMP/src/CMakeFiles/hellocuda.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "../bin/hellocuda.pdb"
-  "../bin/hellocuda"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/hellocuda.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- fastfusion-OpenMP/src/auxiliary/ocv_tools.h
+++
@@ -1,9 +0,0 @@
-#ifndef OCV_TOOLS_H
-#define OCV_TOOLS_H
-
-#include <string>
-#include <opencv2/opencv.hpp>
-
-std::string type_to_string(cv::Mat m);
-
-#endif // OCV_TOOLS_H
